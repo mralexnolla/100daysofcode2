1f4f94c1e35b95fd0fb574aec486e2c4585b6a05
alex <nba4everwanted@gmail.com>
2023-02-19 17:03:48 +0000

    day22 Startint the cart app with the Navbar

---
+++ day22/src/features/cart/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cattItem: [],
+    amount: 0,
+    total: 0,
+    isLoading: true
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+
+})
+
+
+
+export default cartSlice.reducer;
