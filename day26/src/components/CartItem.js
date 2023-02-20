70e5cb5e805f5fd77f5d433608d7540901eafded
alex <nba4everwanted@gmail.com>
2023-02-20 23:05:55 +0000

    day26 worked on the remove action and the button

---
+++ day26/src/components/CartItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {ChevronUp, ChevronDown} from '../icons'
+import { useDispatch } from 'react-redux';
+import { removeItem } from '../features/cart/cartSlice';
+
+
+const CartItem = ({id, title, price, img, amount}) => {
+    const dispatch = useDispatch();
+  return (
+    <article className="cart-item">
+        <img src={img} alt={title}  />
+        <div>
+            <h4>{title}</h4>
+            <h4 className="item-price">{price}</h4>
+            {/** remove button */}
+            <button className="remove-btn" onClick = {() => dispatch(removeItem(id))}>remove</button>
+        </div>
+
+        <div>
+            {/** increase button */}
+            <button className="amount-btn">
+                <ChevronUp />
+            </button>
+
+            {/** Amount */}
+            <p className="amount">{amount}</p>
+
+            {/** decrease amount */}
+            <button className="amount-btn">
+                <ChevronDown />
+            </button>
+        </div>
+    </article>
+  )
+}
+
+export default CartItem
---
+++ day26/src/features/cart/cartSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems'
+
+const initialState = {
+    cartItems: cartItems,
+    amount: 4,
+    total: 0,
+    isLoading: true
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        clearCart: (state) => {
+            state.cartItems = []
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            console.log(itemId)
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+        }
+        
+    }
+    
+    
+
+})
+
+
+
+export default cartSlice.reducer;
+export const {clearCart, removeItem} = cartSlice.actions;
+
+
