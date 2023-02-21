1f4b892156020a13e49d3437ef002b26945ca8d2
alex <nba4everwanted@gmail.com>
2023-02-21 16:13:03 +0000

    day27 worked on the quantity buttons up and dow

---
+++ day27/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CartContainer from './components/CartContainer';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <CartContainer />
+    </div>
+  );
+}
+
+export default App;
