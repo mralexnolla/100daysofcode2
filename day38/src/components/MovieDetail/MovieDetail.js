d83fecaa7c3ec03d53e4b8ba8d3f7650c11940ab
alex <nba4everwanted@gmail.com>
2023-03-05 14:05:56 +0000

    day38 accessing the data into the store using useDispatch

---
+++ day38/src/components/MovieDetail/MovieDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieDetail = () => {
+    return (
+        <div>
+            Movie Details
+        </div>
+    );
+};
+
+export default MovieDetail;
---
+++ day38/src/features/movies/movieSlice.js
@@ -0,0 +1,21 @@
+// now create our slice and actions 
+
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    movies: []
+}
+
+export const movieSlice = createSlice({
+    name: "movies",
+    initialState,
+    reducers: {
+        addMovies: (state, {payload}) => {
+            state.movies = payload
+        }
+    }
+})
+
+export const {addMovies} = movieSlice.actions;
+export default movieSlice.reducer
