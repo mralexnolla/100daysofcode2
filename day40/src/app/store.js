d3d38ef23040bd2cf41f3f55e165d2a2e0a41a46
alex <nba4everwanted@gmail.com>
2023-03-06 14:29:51 +0000

    dat40 displaying the movies on my movie card via props

---
+++ day40/src/app/store.js
@@ -0,0 +1,10 @@
+// first create our store 
+
+import { configureStore } from '@reduxjs/toolkit';
+import movieReducer from '../features/movies/movieSlice'
+
+export const store = configureStore({
+    reducer: {
+        movies : movieReducer,
+    }
+})
---
+++ day40/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Footer.scss"
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div>Movie App</div>
+            <div>@2023,  #thankU4Ex16</div>  
+        </div>
+    );
+};
+
+export default Footer;
